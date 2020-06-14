52bf2b8d12355952ac41b6cbf4a9671340d83eee
Rafael Borgonovi Tauil <rafael@tauil.com.br>
2020-06-14 22:46:14 +0200

    Updates

---
+++ precache-manifest.afbb6bf2987b459569a878ce28e8579b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0ad49d0a0a2104a8fafb5d400d620a2",
+    "url": "/best-office-to-go/index.html"
+  },
+  {
+    "revision": "7d78a5bb2a3a43fb3439",
+    "url": "/best-office-to-go/static/css/main.3ade7153.chunk.css"
+  },
+  {
+    "revision": "d128de2b56ffc7d93887",
+    "url": "/best-office-to-go/static/js/2.bc01f6bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/best-office-to-go/static/js/2.bc01f6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d78a5bb2a3a43fb3439",
+    "url": "/best-office-to-go/static/js/main.bded0a6e.chunk.js"
+  },
+  {
+    "revision": "a4a42f7fdac0801ab3b2",
+    "url": "/best-office-to-go/static/js/runtime-main.75c5915a.js"
+  }
+]);
